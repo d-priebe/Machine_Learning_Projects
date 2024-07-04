5b93f1e9387dda3e8c35e24a24dbf42483feb56a
d-priebe <drewpriebe6@gmail.com>
2024-07-03 21:11:02 -0500

    initial commit

---
+++ Projects/ml_ops/customer_churn_prediction/ data_analysis/EDA.py
@@ -0,0 +1,11 @@
+""" Performs Exploratory Data Analysis"""
+from pathlib import Path
+import pandas as pd
+
+def perform_eda(dataframe):
+    """
+    Args:
+        df (pd.Dataframe): A dataframe input
+        
+    Returns:
+        Plots Visulizations and Saves images."""
